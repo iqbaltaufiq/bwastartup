06433aa1a86fa422259e147a9ce70c6eccf4e5a5
Iqbal Taufiq <iqbaltaufiq@outlook.com>
2023-04-16 14:40:27 +0700

    creating endpoint for login.

--- handler/user.go
+++ handler/user.go
@@ -40,3 +40,29 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	response := helper.APIResponse("Account created successfully.", http.StatusOK, "OK", formatter)
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.UserLoginInput
+	errBind := c.ShouldBindJSON(&input)
+	if errBind != nil {
+		errors := helper.FormatValidationError(errBind)
+		errMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedInUser, errLogin := h.userService.Login(input)
+	if errLogin != nil {
+		errMessage := gin.H{"errors": errLogin.Error()}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedInUser, "tokentokentokentoken")
+	response := helper.APIResponse("Login success", http.StatusOK, "OK", formatter)
+	c.JSON(http.StatusOK, response)
+}
--- user/service.go
+++ user/service.go
@@ -1,11 +1,14 @@
 package user
 
 import (
+	"errors"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
 type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
+	Login(input UserLoginInput) (User, error)
 }
 
 type service struct {
@@ -37,4 +40,25 @@ func (s *service) RegisterUser(input RegisterUserInput) (User, error) {
 	}
 
 	return newUser, nil
+}
+
+func (s *service) Login(input UserLoginInput) (User, error) {
+	email := input.Email
+	password := input.Password
+
+	user, errFind := s.repository.FindByEmail(email)
+	if errFind != nil {
+		return user, errFind
+	}
+
+	if user.Id == 0 {
+		return user, errors.New("no user found for that email")
+	}
+
+	errCompare := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+	if errCompare != nil {
+		return user, errCompare
+	}
+
+	return user, nil
 }
