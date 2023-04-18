d3d65ccb94925d1c34f04e7d4886f2d47267e799
Iqbal Taufiq <iqbaltaufiq@outlook.com>
2023-04-18 16:21:30 +0700

    Creating campaign service. Adding method: FindCampaigns.

--- campaign/service.go
+++ campaign/service.go
@@ -1,1 +1,33 @@
 package campaign
+
+type Service interface {
+	FindCampaigns(userId int) ([]Campaign, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+// fetch campaign from database
+// if client sent user_id,
+// then fetch all campaigns that belong to that user_id
+// if not, then fetch all campaigns from database
+func (s *service) FindCampaigns(userId int) ([]Campaign, error) {
+	if userId != 0 {
+		campaigns, err := s.repository.FindByUserId(userId)
+		if err != nil {
+			return campaigns, err
+		}
+		return campaigns, nil
+	}
+
+	campaigns, err := s.repository.FindAll()
+	if err != nil {
+		return campaigns, err
+	}
+	return campaigns, nil
+}
--- main.go
+++ main.go
@@ -27,20 +27,12 @@ func main() {
 	userRepository := user.NewRepository(db)
 	campaignRepository := campaign.NewRepository(db)
 
-	campaigns, _ := campaignRepository.FindByUserId(4)
-	fmt.Println("debug")
-	fmt.Println("debug")
-	fmt.Println("debug")
-	for _, campaign := range campaigns {
-		fmt.Println(campaign.Name)
-		fmt.Println("length: ", len(campaign.CampaignImages))
-		if len(campaign.CampaignImages) > 0 {
-			fmt.Println(campaign.CampaignImages[0].FileName)
-		}
-	}
-
 	userService := user.NewService(userRepository)
+	campaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewService()
+
+	campaigns, _ := campaignService.FindCampaigns(4)
+	fmt.Println(len(campaigns))
 
 	userHandler := handler.NewUserHandler(userService, authService)
 
